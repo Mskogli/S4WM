1b80692576f584d1174b5d33be85a423e4f07100
mskogli <mathsko@stud.ntnu.no>
2024-01-29 12:21:04 +0100

    Split the S4 code in to two modules

--- s4_nn.py
+++ s4_nn.py
@@ -1,18 +1,14 @@
 import jax
-import jax.numpy as np
+import jax.numpy as jnp
+
 from flax import linen as nn
 from jax.nn.initializers import normal
-from jax.numpy.linalg import eigh, inv, matrix_power
-from jax.scipy.signal import convolve
+from s4_ssm import hippo_initializer, log_step_initializer, kernel_DPLR, discrete_DPLR, causal_convolution, scan_SSM
 
-def log_step_initializer(dt_min=0.001, dt_max=0.1):
-    def init(key, shape):
-        return jax.random.uniform(key, shape) * (
-            np.log(dt_max) - np.log(dt_min)
-        ) + np.log(dt_min)
+"""
+The neural network representation of the S4 model
 
-    return init
-
+"""
 
 def cloneLayer(layer):
     return nn.vmap(
@@ -22,25 +18,6 @@ def cloneLayer(layer):
         variable_axes={"params": 1, "cache": 1, "prime": 1},
         split_rngs={"params": True},
     )
-
-def scan_SSM(Ab, Bb, Cb, u, x0):
-    def step(x_k_1, u_k):
-        x_k = Ab @ x_k_1 + Bb @ u_k
-        y_k = Cb @ x_k
-        return x_k, y_k
-
-    return jax.lax.scan(step, x0, u)
-
-
-def causal_convolution(u, K, nofft=False):
-    if nofft:
-        return convolve(u, K, mode="full")[: u.shape[0]]
-    else:
-        assert K.shape[0] == u.shape[0]
-        ud = np.fft.rfft(np.pad(u, (0, K.shape[0])))
-        Kd = np.fft.rfft(np.pad(K, (0, u.shape[0])))
-        out = ud * Kd
-        return np.fft.irfft(out)[: u.shape[0]]
 
 
 class SequenceBlock(nn.Module):
@@ -88,7 +65,7 @@ class Embedding(nn.Embed):
     @nn.compact
     def __call__(self, x):
         y = nn.Embed(self.num_embeddings, self.features)(x[..., 0])
-        return np.where(x > 0, y, 0.0)
+        return jnp.where(x > 0, y, 0.0)
 
 
 class StackedModel(nn.Module):
@@ -128,12 +105,12 @@ class StackedModel(nn.Module):
             if not self.embedding:
                 x = x / 255.0  # Normalize
             if not self.decode:
-                x = np.pad(x[:-1], [(1, 0), (0, 0)])
+                x = jnp.pad(x[:-1], [(1, 0), (0, 0)])
         x = self.encoder(x)
         for layer in self.layers:
             x = layer(x)
         if self.classification:
-            x = np.mean(x, axis=0)
+            x = jnp.mean(x, axis=0)
         x = self.decoder(x)
         return nn.log_softmax(x, axis=-1)
 
@@ -144,95 +121,6 @@ BatchStackedModel = nn.vmap(
     variable_axes={"params": None, "dropout": None, "cache": 0, "prime": None},
     split_rngs={"params": False, "dropout": True},
 )
-
-def make_HiPPO(N):
-    P = np.sqrt(1 + 2 * np.arange(N))
-    A = P[:, np.newaxis] * P[np.newaxis, :]
-    A = np.tril(A) - np.diag(np.arange(N))
-    return -A
-
-
-@jax.jit
-def cauchy(v, omega, lambd):
-    """Cauchy matrix multiplication: (n), (l), (n) -> (l)"""
-    cauchy_dot = lambda _omega: (v / (_omega - lambd)).sum()
-    return jax.vmap(cauchy_dot)(omega)
-
-
-def kernel_DPLR(Lambda, P, Q, B, C, step, L):
-    # Evaluate at roots of unity
-    # Generating function is (-)z-transform, so we evaluate at (-)root
-    Omega_L = np.exp((-2j * np.pi) * (np.arange(L) / L))
-
-    aterm = (C.conj(), Q.conj())
-    bterm = (B, P)
-
-    g = (2.0 / step) * ((1.0 - Omega_L) / (1.0 + Omega_L))
-    c = 2.0 / (1.0 + Omega_L)
-
-    # Reduction to core Cauchy kernel
-    k00 = cauchy(aterm[0] * bterm[0], g, Lambda)
-    k01 = cauchy(aterm[0] * bterm[1], g, Lambda)
-    k10 = cauchy(aterm[1] * bterm[0], g, Lambda)
-    k11 = cauchy(aterm[1] * bterm[1], g, Lambda)
-    atRoots = c * (k00 - k01 * (1.0 / (1.0 + k11)) * k10)
-    out = np.fft.ifft(atRoots, L).reshape(L)
-    return out.real
-
-def discrete_DPLR(Lambda, P, Q, B, C, step, L):
-    # Convert parameters to matrices
-    B = B[:, np.newaxis]
-    Ct = C[np.newaxis, :]
-
-    N = Lambda.shape[0]
-    A = np.diag(Lambda) - P[:, np.newaxis] @ Q[:, np.newaxis].conj().T
-    I = np.eye(N)
-
-    # Forward Euler
-    A0 = (2.0 / step) * I + A
-
-    # Backward Euler
-    D = np.diag(1.0 / ((2.0 / step) - Lambda))
-    Qc = Q.conj().T.reshape(1, -1)
-    P2 = P.reshape(-1, 1)
-    A1 = D - (D @ P2 * (1.0 / (1 + (Qc @ D @ P2))) * Qc @ D)
-
-    # A bar and B bar
-    Ab = A1 @ A0
-    Bb = 2 * A1 @ B
-
-    # Recover Cbar from Ct
-    Cb = Ct @ inv(I - matrix_power(Ab, L)).conj()
-    return Ab, Bb, Cb.conj()
-
-def make_NPLR_HiPPO(N):
-    # Make -HiPPO
-    nhippo = make_HiPPO(N)
-
-    # Add in a rank 1 term. Makes it Normal.
-    P = np.sqrt(np.arange(N) + 0.5)
-
-    # HiPPO also specifies the B matrix
-    B = np.sqrt(2 * np.arange(N) + 1.0)
-    return nhippo, P, B
-
-def make_DPLR_HiPPO(N):
-    """Diagonalize NPLR representation"""
-    A, P, B = make_NPLR_HiPPO(N)
-
-    S = A + P[:, np.newaxis] * P[np.newaxis, :]
-
-    # Check skew symmetry
-    S_diag = np.diagonal(S)
-    Lambda_real = np.mean(S_diag) * np.ones_like(S_diag)
-    # assert np.allclose(Lambda_real, S_diag, atol=1e-3)
-
-    # Diagonalize S to V \Lambda V^*
-    Lambda_imag, V = eigh(S * -1j)
-
-    P = V.conj().T @ P
-    B = V.conj().T @ B
-    return Lambda_real + 1j * Lambda_imag, P, B, V
 
 class S4Layer(nn.Module):
     N: int
@@ -255,7 +143,7 @@ class S4Layer(nn.Module):
         self.Lambda_im = self.param("Lambda_im", init_A_im, (self.N,))
         # Ensure the real part of Lambda is negative
         # (described in the SaShiMi follow-up to S4)
-        self.Lambda = np.clip(self.Lambda_re, None, -1e-4) + 1j * self.Lambda_im
+        self.Lambda = jnp.clip(self.Lambda_re, None, -1e-4) + 1j * self.Lambda_im
         self.P = self.param("P", init_P, (self.N,))
         self.B = self.param("B", init_B, (self.N,))
         # C should be init as standard normal
@@ -264,7 +152,7 @@ class S4Layer(nn.Module):
         self.C = self.param("C", normal(stddev=0.5**0.5), (self.N, 2))
         self.C = self.C[..., 0] + 1j * self.C[..., 1]
         self.D = self.param("D", nn.initializers.ones, (1,))
-        self.step = np.exp(self.param("log_step", log_step_initializer(), (1,)))
+        self.step = jnp.exp(self.param("log_step", log_step_initializer(), (1,)))
 
         if not self.decode:
             # CNN mode, compute kernel.
@@ -300,7 +188,7 @@ class S4Layer(nn.Module):
 
             # RNN Cache
             self.x_k_1 = self.variable(
-                "cache", "cache_x_k", np.zeros, (self.N,), np.complex64
+                "cache", "cache_x_k", jnp.zeros, (self.N,), jnp.complex64
             )
 
     def __call__(self, u):
@@ -310,57 +198,13 @@ class S4Layer(nn.Module):
             return causal_convolution(u, self.K) + self.D * u
         else:
             # RNN Mode
-            x_k, y_s = scan_SSM(*self.ssm, u[:, np.newaxis], self.x_k_1.value)
+            x_k, y_s = scan_SSM(*self.ssm, u[:, jnp.newaxis], self.x_k_1.value)
             if self.is_mutable_collection("cache"):
                 self.x_k_1.value = x_k
             return y_s.reshape(-1).real + self.D * u
 
 
 S4Layer = cloneLayer(S4Layer)
-
-def init(x):
-    def _init(key, shape):
-        assert shape == x.shape
-        return x
-    return _init
-
-
-def hippo_initializer(N):
-    Lambda, P, B, _ = make_DPLR_HiPPO(N)
-    return init(Lambda.real), init(Lambda.imag), init(P), init(B)
-
-
-def sample(model, params, prime, cache, x, start, end, rng):
-    def loop(i, cur):
-        x, rng, cache = cur
-        r, rng = jax.random.split(rng)
-        out, vars = model.apply(
-            {"params": params, "prime": prime, "cache": cache},
-            x[:, np.arange(1, 2) * i],
-            mutable=["cache"],
-        )
-
-        def update(x, out):
-            p = jax.random.categorical(r, out[0])
-            x = x.at[i + 1, 0].set(p)
-            return x
-
-        x = jax.vmap(update)(x, out)
-        return x, rng, vars["cache"].unfreeze()
-
-    return jax.lax.fori_loop(start, end, jax.jit(loop), (x, rng, cache))[0]
-
-
-def init_recurrence(model, params, init_x, rng):
-    variables = model.init(rng, init_x)
-    vars = {
-        "params": params,
-        "cache": variables["cache"].unfreeze(),
-        "prime": variables["prime"].unfreeze(),
-    }
-    print("[*] Priming")
-    _, prime_vars = model.apply(vars, init_x, mutable=["prime"])
-    return vars["params"], prime_vars["prime"], vars["cache"]
 
 if __name__ == "__main__":
     # For this tutorial, construct a global JAX rng key
